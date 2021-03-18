6afaa17413c9f6eb812ea0681849e049b5ae5307
David-HUMBLOT <humblot.david1@gamil.com>
2021-03-19 01:27:55 +0400

    ok

--- config/config.php
+++ config/config.php
@@ -9,10 +9,10 @@ session_start();
 // connection en ligne (heroku bdd mysql)
 
 // connection bdd en ligne
-$host = "eu-cdbr-west-03.cleardb.net";
-$login = "ba707d338cd120";
-$pwd = "00cbd8e5";
-$db = "heroku_e725779b43b3b92";
+// $host = "eu-cdbr-west-03.cleardb.net";
+// $login = "ba707d338cd120";
+// $pwd = "00cbd8e5";
+// $db = "heroku_e725779b43b3b92";
 // connexion à la BDD
 $db_connect = mysqli_connect($host, $login, $pwd, $db);
 ?>
